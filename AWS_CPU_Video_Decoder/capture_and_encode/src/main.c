d7eb6e59517df1b094e0262e475a77a058634e96
unknown <ashlin.inwood@gmail.com>
2026-02-01 17:16:46 +1300

    Working

--- AWS_CPU_Video_Decoder/capture_and_encode/src/main.c
+++ AWS_CPU_Video_Decoder/capture_and_encode/src/main.c
@@ -1,22 +1,55 @@
-#include "pico/stdlib.h"
 #include <stdio.h>
+#include <string.h>
 #include <inttypes.h>
-#include "hardware/pio.h"
+#include "pico/stdlib.h"
 #include "hardware/dma.h"
-#include "decode.pio.h"
 #include "hardware/clocks.h"
 
+#include "hardware/pio.h"
+#include "decode.pio.h"
+#include "video.pio.h"
+
+#include "font/font.h"
+#include "video.h"
+
+// Capture pins
 #define VSYNC_PIN 2
 #define HSYNC_PIN 3
 #define CCODE_PINS 4 // 4-11
 #define CCLK_PIN 12
 
+// Video output pins
+#define DAC0 20
+#define DAC1 21
+#define DAC2 22
+
+// USB UART Settings
 #define UART_ID uart0
 #define BAUD_RATE 921600
 
-uint16_t screen_data[80 * 29];
+// capture buffer TODO: replace
+// uint16_t screen_data[80 * 29];
+
+extern uint16_t screen_data[SCREEN_HEIGHT * SCREEN_WIDTH];
+
+uint16_t *screen_data_offset = screen_data + 80 * 5;
 
 int dma_capture;
+
+// TODO: Rename
+int dma_chan0;
+int dma_chan1;
+
+
+// Video line buffers
+uint8_t line_a[LINE_SIZE];
+uint8_t line_b[LINE_SIZE];
+volatile uint8_t active_buffer_index = 0;
+uint8_t *line_buffers[] = { line_a, line_b };
+
+uint8_t *active_line; // This is the line that is  being sent out the PIO via DMA
+uint8_t *inactive_line; // This is the line that is  being sent out the PIO via DMA
+
 
 void dma_capture_irq_handler() {
     // Clear interrupt
@@ -25,10 +58,25 @@ void dma_capture_irq_handler() {
     // Reload DMA
     dma_channel_set_write_addr(
         dma_capture,
-        screen_data,
+        screen_data_offset,
         true
     );
 }
+
+
+void dma1_irq_handler() {
+    // Clear DMA1 interrupt
+    dma_hw->ints0 = 1u << dma_chan1;
+
+    // Switch active buffer
+    active_buffer_index ^= 1;
+    active_line = line_buffers[active_buffer_index];
+    inactive_line = line_buffers[active_buffer_index];
+
+    write_scanline(inactive_line);
+}
+
+
 
 int main() {
     set_sys_clock_khz(125000, true);
@@ -36,11 +84,18 @@ int main() {
     uart_init(UART_ID, BAUD_RATE);
 
     // Allow time for the USB uart to connect
-    sleep_ms(3000);
+    // sleep_ms(3000);
+
+    // Setup video
+    video_init();
+    // fill_screen_with_character(0x05, 0);
+    active_line = line_buffers[active_buffer_index];
+    inactive_line = line_buffers[active_buffer_index ^ 1];
 
     printf("Running video sample");
-    // Use PIO0 for capture of video data
-    PIO pio_zero = pio0;
+
+    PIO pio_zero = pio0; // Use PIO0 for capture of video data
+    PIO pio_one = pio1;  // Use PIO1 for video
 
     // -------------------------------
     // Load all programs in PIO memory
@@ -49,12 +104,16 @@ int main() {
     uint offset_raster = pio_add_program(pio_zero, &raster_program);
     uint offset_capture = pio_add_program(pio_zero, &capture_program);
 
+    uint offset_dma = pio_add_program(pio_one, &dma_program);
+
     // -------------------------------
     // Allocate state machines
     // -------------------------------
     uint sm_vsync   = pio_claim_unused_sm(pio_zero, true);
     uint sm_raster  = pio_claim_unused_sm(pio_zero, true);
     uint sm_capture = pio_claim_unused_sm(pio_zero, true);
+
+    uint sm_dma = pio_claim_unused_sm(pio_one, true);
 
     // -------------------------------
     // CHARACTER_CAPTURE program
@@ -81,12 +140,17 @@ int main() {
     gpio_set_dir(VSYNC_PIN, GPIO_IN);
     vsync_program_init(pio_zero, sm_vsync, offset_vsync, VSYNC_PIN);
 
+    // -------------------------------
+    // VIDEO program
+    // -------------------------------
+    dma_program_init(pio_one, sm_dma, offset_dma, DAC0);
+
     // Stop the sm
     pio_sm_set_enabled(pio_zero, sm_capture, false);
     pio_sm_set_enabled(pio_zero, sm_raster, false);
     pio_sm_set_enabled(pio_zero, sm_vsync, false);
 
-    // Set up DMA
+    // Set up DMA for capture
     dma_capture = dma_claim_unused_channel(true);
     dma_channel_config dma_capture_cf = dma_channel_get_default_config(dma_capture);
 
@@ -99,7 +163,7 @@ int main() {
     dma_channel_configure(
         dma_capture,
         &dma_capture_cf,
-        screen_data,                // Write address
+        screen_data_offset,                // Write address
         &pio_zero->rxf[sm_capture], // Read address (the address of the pio's fifo)
         80 * 29,                    // Number of transfers
         false                       // Autostart
@@ -110,9 +174,52 @@ int main() {
     irq_set_exclusive_handler(DMA_IRQ_0, dma_capture_irq_handler);
     irq_set_enabled(DMA_IRQ_0, true);
 
-    printf("\033[?25l");  // hide cursor
+
+
+    // Setup DMA for video
+    dma_chan0 = dma_claim_unused_channel(true);
+    dma_chan1 = dma_claim_unused_channel(true);
+
+    dma_channel_config c0 = dma_channel_get_default_config(dma_chan0);
+
+    channel_config_set_transfer_data_size(&c0, DMA_SIZE_8);  // One byte at a time
+    channel_config_set_read_increment(&c0, true);
+    channel_config_set_write_increment(&c0, false);
+    channel_config_set_dreq(&c0, pio_get_dreq(pio_one, sm_dma, true)); // Send byte as pio needs it
+    channel_config_set_chain_to(&c0, dma_chan1); // On the end of DMA0 start DMA1
+
+    // Set up DMA0
+    dma_channel_configure(
+        dma_chan0,
+        &c0,
+        &pio_one->txf[sm_dma], // Write address (the address of the pio's fifo)
+        line_blank_field,      // Address of the data to be read
+        1000,                 // Number of transfers
+        false                 // Autostart
+    );
+
+    dma_channel_config c1 = dma_channel_get_default_config(dma_chan1);
+    channel_config_set_transfer_data_size(&c1, DMA_SIZE_32);    // Size of a pointer
+    channel_config_set_read_increment(&c1, false);              // Read the same address
+    channel_config_set_write_increment(&c1, false);             // Write the same address
+    channel_config_set_chain_to(&c1, dma_chan0);                // On the end of DMA1 start DMA0
+
+    dma_channel_configure(
+        dma_chan1,
+        &c1,
+        &dma_hw->ch[dma_chan0].read_addr, // Write to the "source address" for DMA0
+        &active_line,
+        1, // Move just the pointer
+        false
+    );
+
+    // Tirgger a irq at the end of the "reset" transfer
+    dma_channel_set_irq1_enabled(dma_chan1, true);
+    irq_set_exclusive_handler(DMA_IRQ_1, dma1_irq_handler);
+    irq_set_enabled(DMA_IRQ_1, true);
 
     // Start DMA
+    dma_start_channel_mask(1u << dma_chan1);
     dma_start_channel_mask(1u << dma_capture);
 
     // Start PIO
@@ -120,6 +227,9 @@ int main() {
     raster_program_init(pio_zero, sm_raster, offset_raster, HSYNC_PIN);
     vsync_program_init(pio_zero, sm_vsync, offset_vsync, VSYNC_PIN);
 
+    // Debug screen
+    #if 0
+    printf("\033[?25l");  // hide cursor
     while (1) {
         // Print capture
         printf("\033[H"); // just move to top-left
@@ -129,11 +239,16 @@ int main() {
         for(uint i = 0; i < 29; i++) {
             putchar('|');
             for(uint j = 0; j < 80; j++) {
-                uint index = (i * 80) + j;
-                unsigned char c = 0xff & screen_data[index];
 
-                if ((c >= 32 && c <= 126) || c >= 128) {
-                    putchar(c);
+                uint index = (i * 80) + j;
+
+                uint16_t code = screen_data[index];
+                uint8_t ac = 0x0F & (uint8_t)(code >> 8);
+                uint8_t cc = (uint8_t)(code & 0xFF);
+
+
+                if ((cc >= 32 && cc <= 126) || cc >= 128) {
+                    putchar(cc);
                 } else {
                     putchar(' ');
                 }
@@ -143,6 +258,9 @@ int main() {
         }
         printf("+--------------------------------------------------------------------------------+\n\n");
     }
+    #endif
+
+    while (1) {}
 
     // Clean up
     pio_remove_program_and_unclaim_sm(&vsync_program, pio_zero, sm_vsync, offset_vsync);
---
+++ AWS_CPU_Video_Decoder/capture_and_encode/src/video.h
@@ -0,0 +1,135 @@
+#ifndef VIDEO_H
+#define VIDEO_H
+
+#include "font/font.h"
+
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 48
+
+#define VOLTS_0 0x00
+#define VOLTS_2 0x04
+#define VOLTS_3 0x02
+#define VOLTS_4 0x01
+#define VOLTS_5 0x03
+#define VOLTS_6 0x05
+#define VOLTS_7 0x06
+#define VOLTS_M 0x07
+
+#define VOLTS_0_DOUBLE 0x00
+#define VOLTS_2_DOUBLE 0x24
+#define VOLTS_3_DOUBLE 0x12
+#define VOLTS_4_DOUBLE 0x09
+#define VOLTS_5_DOUBLE 0x1B
+#define VOLTS_6_DOUBLE 0x2D
+#define VOLTS_7_DOUBLE 0x36
+#define VOLTS_M_DOUBLE 0x3F
+
+#define ZERO_BRIGHT VOLTS_3
+#define FULL_BRIGHT VOLTS_M
+#define HALF_BRIGHT VOLTS_6
+
+#define ZERO_BRIGHT_D VOLTS_3_DOUBLE
+#define FULL_BRIGHT_D VOLTS_M_DOUBLE
+#define HALF_BRIGHT_D VOLTS_6_DOUBLE
+
+#define LINE_SIZE 1000
+#define HALF_LINE_SIZE 500
+
+extern uint8_t line_broad_field[LINE_SIZE]; // Green
+extern uint8_t line_short_field[LINE_SIZE]; // Orange
+extern uint8_t line_blank_field[LINE_SIZE]; // White
+extern uint8_t line_broad_short_field[LINE_SIZE]; // Green/Orange
+extern uint8_t line_short_broad_field[LINE_SIZE]; // Orange/Green
+
+void video_init();
+
+void copy_character_line(uint8_t *disaply_line, uint32_t screen_row, uint32_t glyph_row);
+
+void fill_screen_with_character(uint8_t character_code, uint8_t attribute_code);
+
+void screen_set_character(uint8_t character_code, uint8_t attribute_code, uint8_t row, uint8_t column);
+
+void copy_buffer_to_screen(const uint16_t *buffer, size_t size, uint8_t start_row);
+
+static inline void write_scanline(uint8_t *inactive_line) {
+    static int line_count;
+
+    switch (line_count) {
+        // Broad field
+        case 1:
+        case 2:
+        case 314:
+        case 315:
+            memcpy(inactive_line, line_broad_field, LINE_SIZE);
+        break;
+
+        // Mixed broad short field
+        case 3:
+            memcpy(inactive_line, line_broad_short_field, LINE_SIZE);
+        break;
+
+        // Mixed short broad field
+        case 313:
+            memcpy(inactive_line, line_short_broad_field, LINE_SIZE);
+        break;
+
+        // Short field
+        case 4:
+        case 5:
+        case 311:
+        case 312:
+        case 316:
+        case 317:
+        case 624:
+        case 625:
+            memcpy(inactive_line, line_short_field, LINE_SIZE);
+        break;
+
+        // Blank screen data
+        case 6 ... 34:
+        case 319 ... 346:
+        case 612 ... 622:
+        case 299 ... 310:
+            memcpy(inactive_line, line_blank_field, LINE_SIZE);
+        break;
+
+        // Even screen data
+        case 35 ... 298: {
+            uint32_t feild_line = ((line_count - 35) * 2) + 1;
+            uint32_t font_row = feild_line % FONT_HEIGHT;
+            uint32_t screen_row = feild_line / FONT_HEIGHT;
+            copy_character_line(inactive_line, screen_row, font_row);
+            break;
+        }
+
+        // Odd screen data
+        case 347 ... 611: {
+            uint32_t feild_line = ((line_count - 347) * 2);
+            uint32_t font_row = feild_line % FONT_HEIGHT;
+            uint32_t screen_row = feild_line / FONT_HEIGHT;
+            copy_character_line(inactive_line, screen_row, font_row);
+            break;
+        }
+
+        // Odd half line data (technically ment to have data, but I will blank it)
+        case 318:
+            memcpy(inactive_line, line_short_field, HALF_LINE_SIZE);
+            memcpy(inactive_line + HALF_LINE_SIZE, line_blank_field, HALF_LINE_SIZE);
+        break;
+
+        case 623:
+            memcpy(inactive_line, line_blank_field, HALF_LINE_SIZE);
+            memcpy(inactive_line + HALF_LINE_SIZE, line_short_field, HALF_LINE_SIZE);
+        break;
+
+        default:
+            printf("Not in the number range %d", line_count);
+        break;
+    }
+
+    if(++line_count >= 626) {
+        line_count = 1;
+    }
+}
+
+#endif /* VIDEO_H */
